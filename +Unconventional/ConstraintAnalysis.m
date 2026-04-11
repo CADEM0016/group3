12e586310d821f769257d470e0761bd8f61a9020
ng25733 <ng25733@bristol.ac.uk>
2026-04-11 17:07:08 +0100

    Updated constraint analysis

--- +Unconventional/ConstraintAnalysis.m
+++ +Unconventional/ConstraintAnalysis.m
@@ -1,6 +1,6 @@
 function [ThrustToWeightRatio,WingLoading] = ConstraintAnalysis(obj)
 
-%% estimate T/W and W/S from constraint analysis
+%% estimate T/W and W/S from constraint analysis (Old Code)
 % for now just setting to those of B777
 % ---------------------- TODO -----------------------
 % --------- update with constraint analysis ---------
@@ -17,38 +17,43 @@ function [ThrustToWeightRatio,WingLoading] = ConstraintAnalysis(obj)
 
 %% estimate T/W and W/S from constraint analysis
 
-TL = 100:1000;  % Wing Loading (W/S)
-
+%TL = 100:1000;  % Wing Loading (W/S)
 %% Take-Off Values
 g = 1;         % Gravity
-CL = 2.5;      % Coefficient of Lift  
-Cd = 0.12;     % Coefficient of Drag
+CL = 0.8;      % Coefficient of Lift  
+Cd = 0.03;     % Coefficient of Drag
 Q = 34.6;      % Dynamic pressure at VLOF
-CLMax = 1.3;   % Max Coefficient of Lift
+CLMax = 1.5;   % Max Coefficient of Lift
 Sg = 2500;     % Ground Run (m)
 u = 0.04;      % Ground Friction Constant
 
-TOV = ((1.21/(g*Sg*CLMax*1.225)*TL) + ((1/2)*(Cd/CLMax))+((1/2)*u)) %Take Off Values
-
+%TOV = ((1.21/(g*Sg*CLMax*1.225)*TL) + ((0.605/CLMax)*(Cd + (u*CL))) + u );  %Take Off Values
 %% Climb Values
 Vv = 15.6;     % Vertical Speed (m/s)
-As = 126;      % Airspeed (m/s)
-q = 1128;      % Dynamic Pressure at the Selected Airspeed and Altitude (N/M)
+As = 128.61;   % Airspeed @ 250kts (m/s)
+q = 1128;      % Dynamic Pressure at the Selected Airspeed and Altitude 20000ft (N/M)
+CDmin = 0.02;   % Minimum Coefficient of Drag
+AR = 10.5;      % Aspect Ratio
+e = 0.8 ;      % Oswald Efficiency Factor
+k= 1/(pi*e*AR); % Induced Drag Factor
 
-CV = (Vv/As) + ((q./TL)*0.0396) + ((0.0433/q)*TL);
+%CV = (Vv/As) + ((q./TL)*CDmin) + ((k/q)*TL); % Climb Values
 
-%% Landing
+%% Landing Values
 Vapp = 74.6;    % Approach Velocity
 Vs = 1.3*Vapp;  % Stl Velocity
 Density = 1.225;% Density At Sea Level
 
-L = ((1/2) * Density * (Vs^2) * CLMax) / 9.81; 
+L = ((1/2) * Density * (Vs^2) * CLMax)/9.81; % Landing Values
 
+%% Sustained Turn
+n = 1/(cos(30*pi/180)); % Bank Angle
+qt = 3407; % Dynamic Pressure at the Selected Airspeed and Altitude 10000ft
+
+%ST = qt*((CDmin./TL) + k*(((n/qt)^2)*TL)); % Sustained Turn Values
 %% Plots
-plot(TL , TOV,'b', TL,CV, 'm' , 'LineWidth', 2)
-xline(L,'k', "LineWidth",3)
-xlabel('Wing Loading')
-ylabel('Thrust-to-Weight Ratio')
-title("Constraint Analysis")
-
-end
+%plot(TL , TOV, TL,CV, 'm' , TL, ST, 'c',   'LineWidth', 2)
+%xline(L,'k', "LineWidth",3)
+%xlabel('Wing Loading (KG/M^2)')
+%ylabel('Thrust-to-Weight Ratio')
+%title("Constraint Analysis")
